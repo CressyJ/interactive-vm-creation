d22295dfb524196bce9696edb28568f98cf95047
cressyj <james@mpcressy.com>
2018-05-16 14:28:30 +0100

    Updating with basic functionality.

--- InteractiveVMCreation/Form1.Designer.cs
+++ InteractiveVMCreation/Form1.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.miniConsole = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(225, 322);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Launch VM";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // miniConsole
+            // 
+            this.miniConsole.AcceptsReturn = true;
+            this.miniConsole.BackColor = System.Drawing.SystemColors.WindowText;
+            this.miniConsole.Cursor = System.Windows.Forms.Cursors.Default;
+            this.miniConsole.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.miniConsole.ForeColor = System.Drawing.SystemColors.Window;
+            this.miniConsole.Location = new System.Drawing.Point(13, 33);
+            this.miniConsole.Multiline = true;
+            this.miniConsole.Name = "miniConsole";
+            this.miniConsole.ReadOnly = true;
+            this.miniConsole.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.miniConsole.Size = new System.Drawing.Size(309, 82);
+            this.miniConsole.TabIndex = 1;
+            this.miniConsole.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Output:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(334, 361);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.miniConsole);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Interactive Azure VM Creation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox miniConsole;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- InteractiveVMCreation/Form1.cs
+++ InteractiveVMCreation/Form1.cs
@@ -7,6 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.Compute.Fluent.Models;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+using Microsoft.Azure.Management.Compute.Fluent;
 
 namespace InteractiveVMCreation
 {
@@ -15,6 +20,34 @@ namespace InteractiveVMCreation
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var authProperties = "C:\\Users\\" + Environment.UserName + "\\azure\\auth.properties";
+
+            IAzure azure = Azure.Authenticate(authProperties).WithDefaultSubscription();
+
+            var linuxVM = azure.VirtualMachines.Define("linux")
+                .WithRegion(Microsoft.Azure.Management.ResourceManager.Fluent.Core.Region.UKWest)
+                .WithNewResourceGroup("test-vm-app")
+                .WithNewPrimaryNetwork("10.0.0.0/28")
+                .WithPrimaryPrivateIPAddressDynamic()
+                .WithNewPrimaryPublicIPAddress("cressyjtest")
+                .WithPopularLinuxImage(KnownLinuxVirtualMachineImage.CentOS7_2)
+                .WithRootUsername("james")
+                .WithRootPassword("SuperSecret-123();")
+                .WithSize(VirtualMachineSizeTypes.StandardA1v2);
+            miniConsole.AppendText("VM Properties Set\r\n");
+            try
+            {
+                var machine = azure.VirtualMachines.Create(linuxVM);
+                miniConsole.AppendText("VM Created\r\n");
+            }
+            catch (Exception exc)
+            {
+                miniConsole.AppendText(exc.Message + "\r\n");
+            }
         }
     }
 }
